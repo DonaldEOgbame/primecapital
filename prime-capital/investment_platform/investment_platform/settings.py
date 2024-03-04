ab0337e29442ed42f4d2c6695250d2cb9200c961
Donald Emma Ogbame <144448621+DonaldEOgbame@users.noreply.github.com>
2024-03-04 13:50:54 +0100

    Update settings.py
    Set allowed host for test server
    Set DEBUG to False

--- prime-capital/investment_platform/investment_platform/settings.py
+++ prime-capital/investment_platform/investment_platform/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-s6^y9m-cqyt@dw1014jg$(=bs!c3yxcnw$&pzcl_v545b2ea&h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['donald001.pythonanywhere.com']
 
 
 # Application definition
